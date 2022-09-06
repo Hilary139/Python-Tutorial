f91403159b55a78082fa4e16c5a12146c87f241a
Hilary Aghasili <Hilary@Hilarys-MacBook-Air.local>
2022-09-06 12:34:11 +0100

    better

--- Intermediate/object oriented programming/encapsulation & abstaction.py
+++ Intermediate/object oriented programming/encapsulation & abstaction.py
@@ -7,17 +7,33 @@ class SoftwareDeveloper():
         self.name = name
         self.age = age
         self._salary = None
-        self._num_bugs_solved = 10
+        self._num_bugs_solved = 0
+
+
+    def code(self):
+        self._num_bugs_solved +=1    
 
 # Getter
     def get_salary(self):
         return self._salary
 # setter
-    def set_salary(self, value):
-        self._salary = value        
+    def set_salary(self, base_value):
+        self._salary = self._calculate_salary(base_value)
 
+
+    def _calculate_salary(self, base_value):
+        if self._num_bugs_solved < 10:
+            return base_value
+        if self._num_bugs_solved < 100:
+            return base_value * 2 
+        return base_value * 3        
+    
 sd= SoftwareDeveloper('Max', 20)
 print(sd.name, sd.age, sd._num_bugs_solved)
 
+for i in range(70):
+    sd.code()
+
 sd.set_salary(60000)
 print(sd.get_salary())
+
