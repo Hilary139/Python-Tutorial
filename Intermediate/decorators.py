dcc01f9d111e07220ba5bce1a3b241724649cdf9
Hilary Aghasili <Hilary@Hilarys-MacBook-Air.local>
2022-09-06 13:03:29 +0100

    better

--- Intermediate/decorators.py
+++ Intermediate/decorators.py
@@ -1,12 +1,22 @@
 
 #? A decorator is a design parttern in python that allows a user to add new functionality to an exiting object without modifying its structure
-
+# types: function decorators and class decorators
 #todo: They are called before defining a function.
 
-#* examples:
-@uppercase_decorator
-def upper_case():
-    return 'abcdefghijklmnopqrstuvwxyz'
-upper_case()    
+#* examples: creating a decorator function to be used.
+def decorate_func(func):
+
+    def wrapper():
+        print('start')
+        func()
+        print('end')   
+    return wrapper
+
+
+@decorate_func    # This is a decorator function
+def name():
+    print('alex')
+
+name()    
 
    
